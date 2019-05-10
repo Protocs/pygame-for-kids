f80c20b1c6363a9cf98bbc0d4791bb9c551d5ed5
nikitakosatka <nikitakosatka@yandex.ru>
2019-05-10 22:55:26 +0500

    Ускорение игрока

--- pygameforkids/player.py
+++ pygameforkids/player.py
@@ -10,11 +10,25 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = 180
         self.rect.y = 800
+        self.speeding = 5
+        self.moving = True
 
     def update(self):
-        self.rect.y -= 2
+        self.key_handle()
 
+        if self.speeding > 0 and self.moving:
+            self.rect.y -= round(self.speeding)
+            self.speeding -= 0.03
+        else:
+            self.moving = False
+
+        if not self.moving:
+            self.speeding += 0.03
+            self.rect.y += round(self.speeding)
+
+    def key_handle(self):
         if pygame.key.get_pressed()[pygame.K_LEFT]:
-            self.rect.x -= 1
+            self.rect.x -= 2
         if pygame.key.get_pressed()[pygame.K_RIGHT]:
-            self.rect.x += 1
+            self.rect.x += 2
+
