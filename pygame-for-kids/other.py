9f46fc9bcfcdc7c3adca0bf4eacfbb90e6ba7f32
nikitakosatka <nikitakosatka@yandex.ru>
2019-05-10 19:46:17 +0500

    Файл "other.py" с функцией загрузки изображений и остановки программы

---
+++ pygame-for-kids/other.py
@@ -0,0 +1,12 @@
+import pygame
+from os import path
+
+
+def load_image(filename):
+    fullname = path.join("data", "images", filename)
+    return pygame.image.load(fullname).convert_alpha()
+
+
+def terminate():
+    pygame.quit()
+    exit()
